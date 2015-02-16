9dba9200d990d1639af751dcccce1b1f1ce6236f
kaidi <ykdacd123@gmail.com>
2015-02-16 10:30:48 +0800

    start to work again

--- scatter-asm/ssasm.h
+++ scatter-asm/ssasm.h
@@ -1,17 +1,19 @@
 #ifndef  SSASM_H
-#define SSAMS_H
+#define  SSAMS_H
+
+#define MAX_INDENT_SIZE 4096
 
 int IsCharNumeric(char cChar);
 int IsCharWhiteSpace(char cChar);
 int IsCharIdent(char cChar);
 int IsCharDelimiter(char cChar);
 
-int IsStringInt(char* str);
-int IsStringFloat(char* str);
-int IsStringWhitespace(char* str);
-int IsStringIdent(char* str);
+int IsStringInt(const char* str);
+int IsStringFloat(const char* str);
+int IsStringWhitespace(const char* str);
+int IsStringIdent(const char* str);
 
-void ExitOnError(char* errorMessage);
+void ExitOnError(const char* errorMessage);
 void Exit();
 void ShutDown();
 
@@ -49,12 +51,12 @@ typedef struct _ScriptHeader
 
 typedef struct _FuncNode
 {
-	int iIndex;
+	int  iIndex;
 	char strName[MAX_INDENT_SIZE];
 	int  iEntryPoint;
-	int iParamCount;
-	int iLocalDataSize;
-};
+	int  iParamCount;
+	int  iLocalDataSize;
+}FuncNode;
 
 
 #endif
