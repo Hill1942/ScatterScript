61509bcfcf36ab72d89f113121a4dd5a754d936b
Hill1942 <ykdacd123@gmail.com>
2015-03-02 22:27:49 +0800

    add type for our ssvm

---
+++ scatter-vm/ss_lang_vm.cpp
@@ -0,0 +1,1 @@
+#include "sslang_vm.h"
---
+++ scatter-vm/sslang_vm.h
@@ -0,0 +1,73 @@
+#ifndef SSLANG_VM_H
+#define SSLANG_VM_H
+
+typedef struct _Value
+{
+	int iType;
+	union
+	{
+		int   iIntLiteral;
+		float fFloatLiteral;
+		char* strStringLiteral;
+		int   iStackIndex;
+		int   iInstrIndex;
+		int   iFuncIndex;
+		int   iHostAPICallIndex;
+		int   iReg;
+	};
+	int iOffsetIndex;
+}Value;
+
+typedef struct _Instr
+{
+	int iOpcode;
+	int iOpCount;
+	Value* pOplist;
+}Instr;
+
+typedef struct _InstrStream
+{
+	Instr* pInstr;
+	int iSize;
+	int iCurrentInstr;
+}InstrStream;
+
+typedef struct _RuntimeStack
+{
+	Value* pElement;
+	int iSize;
+	int* iTopIndex;
+	int iFrameIndex;
+}RuntimeStack;
+
+typedef struct _Func
+{
+	int iEntryPoint;
+	int iParamCount;
+	int iLocalDataSize;
+	int iStackFrameSize;
+}Func;
+
+typedef struct _HostAPICallTable
+{
+	char** ppStrCalls;
+	int    iSize;
+}HostAPICallTable;
+
+typedef struct _Script
+{
+	int iGlobalDataSize;
+	int iIsMainFuncExist;
+	int iMainFuncIndex;
+
+	int iIsPaused;
+	int iPauseEndTime;
+
+	Value _RetVal;
+
+	InstrStream instrStream;
+	RuntimeStack stack;
+	Func* pFuncTable;
+	HostAPICallTable hostAPICallTable;
+};
+#endif
